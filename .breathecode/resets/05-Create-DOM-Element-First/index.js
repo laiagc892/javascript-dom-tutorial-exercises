5510764089017f658ac56079f57ad13419ec05dc
Laia Guillén Carulla <laiagc892@gmail.com>
2021-03-26 10:43:39 +0000

    exs 1-5

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,7 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-let par = document.createElement("p");
-par.style.background = "yellow";
-par.innerHTML = "Hello World";
-document.body.appendChild(par);
