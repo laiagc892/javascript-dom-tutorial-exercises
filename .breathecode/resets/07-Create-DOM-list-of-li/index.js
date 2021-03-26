ca9075dc01bb5bcaaea37c5819e707cbcc6dff9f
Laia Guillén Carulla <laiagc892@gmail.com>
2021-03-26 11:04:20 +0000

    exs 6-7

--- .breathecode/resets/07-Create-DOM-list-of-li/index.js
+++ .breathecode/resets/07-Create-DOM-list-of-li/index.js
@@ -2,11 +2,5 @@ let beginning = "<ul>";
 let liststring = "";
 let ending = "</ul>";
 
-let liBeg = "<li>";
-let liEnd = "</li>";
-liststring += liBeg + "First Item" + liEnd;
-liststring += liBeg + "Second Item" + liEnd;
-liststring += liBeg + "Third Item" + liEnd;
-
 // do not modify after this line
 document.querySelector("#myDiv").innerHTML = beginning + liststring + ending;
